b5ec1ad7892be9988c8795aca64203732e7a92e7
wjakub072 <47480254+wjakub072@users.noreply.github.com>
2019-06-01 12:24:15 +0200

    changed
    new

--- addogl.php
+++ addogl.php
@@ -16,16 +16,6 @@
     <title>Sprzątando</title>
 </head>
 <body>
-<?php
-session_start();
-
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: login.php");
-    exit;
-} else {
-    echo "jd";
-}
-?>
     <div class="container">
 
         <!-- UPPER PASEK -->
@@ -85,7 +75,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
               </nav>
             
         <!-- FORMULARZ -->
-    <form action="" method="POST">
         <div class="formularz"> 
             <p> Tytuł ogłoszenia: </p>
             <div class="row">
@@ -94,7 +83,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                        <div class="input-group-prepend">
                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-file"></i></span>
                        </div>
-                   <input type="text" class="form-control" placeholder="Nazwa ogłoszenia" aria-label="Title" aria-describedby="basic-addon1" name="ad_name">
+                   <input type="text" class="form-control" placeholder="Nazwa ogłoszenia" aria-label="Title" aria-describedby="basic-addon1">
                </div>
                </div>
            </div>
@@ -106,7 +95,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><i class="fas fa-user"></i></span>
                         </div>
-                    <input type="text" class="form-control" placeholder="Imię i nazwisko" aria-label="Username" aria-describedby="basic-addon1" name="name">
+                    <input type="text" class="form-control" placeholder="Nazwa Użytkownika" aria-label="Username" aria-describedby="basic-addon1">
                 </div>
                 </div>
             </div>
@@ -118,7 +107,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><i class="fas fa-city"></i></span>
                         </div>
-                    <input type="text" class="form-control" placeholder="Miejscowość" aria-label="Username" aria-describedby="basic-addon1" name="city">
+                    <input type="text" class="form-control" placeholder="Miejscowość" aria-label="Username" aria-describedby="basic-addon1">
                 </div>
                 </div>
             </div>
@@ -129,7 +118,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1"><i class="fas fa-home"></i></span>
                         </div>
-                    <input type="text" class="form-control" placeholder="Ulica i nr domu" aria-label="Username" aria-describedby="basic-addon1" name="adress">
+                    <input type="text" class="form-control" placeholder="Ulica i nr domu" aria-label="Username" aria-describedby="basic-addon1">
                 </div>
                 </div>
             </div>
@@ -141,7 +130,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="basic-addon1"><i class="fas fa-phone"></i></span>
                             </div>
-                            <input type="text" class="form-control" placeholder="Numer Telefonu" aria-label="Username" aria-describedby="basic-addon1" name="telephone">
+                            <input type="text" class="form-control" placeholder="Numer Telefonu" aria-label="Username" aria-describedby="basic-addon1">
                         </div>
                     </div>
             </div>
@@ -152,7 +141,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
                                 </div>
-                                <input type="mail" class="form-control" placeholder="Adres e-mail" aria-label="Username" aria-describedby="basic-addon1" name="email">
+                                <input type="mail" class="form-control" placeholder="Adres e-mail" aria-label="Username" aria-describedby="basic-addon1">
                             </div>
                 </div>
             </div>
@@ -163,7 +152,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                     <div class="input-group mb-3">
                             <div class="input-group-prepend">
                               <div class="input-group-text">
-                                <input type="checkbox" aria-label="Checkbox for following text input" name="kitchen" value="1">
+                                <input type="checkbox" name="p-kuchnia" aria-label="Checkbox for following text input">
                               </div>
                             </div>
                                 <div class="form-control">
@@ -176,13 +165,13 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                    <div class="input-group mb-3">
                        <div class="input-group-prepend">
                          <div class="input-group-text">
-                           <input type="checkbox" aria-label="Checkbox for following text input" name="bathroom">
+                           <input type="checkbox" name="p-lazieniki" aria-label="Checkbox for following text input">
                          </div>
                        </div>
                        <div class="form-control">
                                Łazienka
                        </div>
-                       <select class="custom-select" id="p-lazieniki-ile" name="ile_lazienek">
+                       <select class="custom-select" id="p-lazieniki-ile">
                                <option selected> Ile? </option>
                                <option value="1">1</option>
                                <option value="2">2</option>
@@ -197,13 +186,13 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                               <div class="input-group-text">
-                                <input type="checkbox" aria-label="Checkbox for following text input" name="bedroom">
+                                <input type="checkbox" name="p-sypialnia" aria-label="Checkbox for following text input">
                               </div>
                             </div>
                             <div class="form-control">
                                     Sypialnia
                             </div>
-                            <select class="custom-select" id="p-sypialnia-ile" name="ile_sypialni">
+                            <select class="custom-select" id="p-sypialnia-ile">
                                     <option selected> Ile? </option>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
@@ -223,7 +212,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                             <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                       <div class="input-group-text">
-                                        <input type="checkbox" aria-label="Checkbox for following text input" name="garage" value="1">
+                                        <input type="checkbox" name="p-garaż" aria-label="Checkbox for following text input">
                                       </div>
                                     </div>
                                         <div class="form-control">
@@ -238,7 +227,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                     <div class="input-group mb-3">
                             <div class="input-group-prepend">
                               <div class="input-group-text">
-                                <input type="checkbox" aria-label="Checkbox for following text input" name="window" value="1">
+                                <input type="checkbox" aria-label="Checkbox for following text input">
                               </div>
                             </div>
                                 <div class="form-control">
@@ -251,7 +240,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                   <div class="input-group-text">
-                                    <input type="checkbox" aria-label="Checkbox for following text input" name="podloga" value="1">
+                                    <input type="checkbox" aria-label="Checkbox for following text input">
                                   </div>
                                 </div>
                                     <div class="form-control">
@@ -264,7 +253,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                   <div class="input-group-text">
-                                    <input type="checkbox" aria-label="Checkbox for following text input" name="pranie_zaslon" value="1">
+                                    <input type="checkbox" aria-label="Checkbox for following text input">
                                   </div>
                                 </div>
                                     <div class="form-control">
@@ -283,7 +272,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         <div class="input-group-prepend">
                             <label class="input-group-text" for="inputGroupSelect01">Wybierz</label>
                         </div>
-                            <select class="custom-select" id="inputGroupSelect01" name="payment_method">
+                            <select class="custom-select" id="inputGroupSelect01">
                               <option value="1">Gotówka</option>
                               <option value="2">Płatność przelewem</option>
                         </select>
@@ -297,7 +286,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                           <div class="input-group-prepend">
                               <span class="input-group-text" id="basic-addon1"><i class="fas fa-money-bill-wave"></i></span>
                           </div>
-                          <input type="text" class="form-control" placeholder="Cena" aria-label="Username" aria-describedby="basic-addon1" name="price">
+                          <input type="text" class="form-control" placeholder="Cena" aria-label="Username" aria-describedby="basic-addon1">
                       </div>
                   </div>
               </div>
@@ -305,85 +294,12 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
               <div class="row">
                   <div class="col-lg-6">
-                      <input type="submit" class="btn btn-light submit-btn" value="Zatwierdz">
+                      <input type="submit" class="btn btn-light submit-btn" value="Zatwierdz"></input>
                   </div>
               </div>
 
               
         </div>
-        
-        </form>
-        <?php
-    require "config.php";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $field_value = new stdClass();
-
-        foreach ($_POST as $key => $value) {
-            # code...
-            $field_value->$key =$value;
-        }
-        echo $field_value->podloga;
-
-        $sql = "INSERT INTO ogloszenia (ad_id,  ad_name, username, city, adress,telephone,email,kitchen,bathroom,bedroom,garage,window,podloga,pranie_zaslon, payment, price) VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        if ($stmt = $mysqli->prepare($sql)){
-            $stmt->bind_param("ssssisiiiiiiiii", $field_value->ad_name,$field_value->name,$field_value->city,$field_value->adress,$field_value->telephone, $field_value->email,$field_value->kitchen, $field_value->ile_lazienek,$field_value->ile_sypialni, $field_value->garage, $field_value->window, $field_value->podloga, $field_value->pranie_zaslon, $field_value->payment_method, $field_value->price);
-
-            if ($stmt->execute()){
-                $stmt->store_result();
-            }else{
-                echo $stmt->error;
-            }
-
-        }else{
-            echo " błąd po prepare";
-        }
-
-        // $sql1 = "INSERT INTO ogloszenia (podloga) VALUES (?)";
-        // if($stmt = $mysqli->prepare($sql1)){
-        //     $stmt->bind_param("i", $field_value->podloga);
-
-        //     if($stmt->execute()){
-        //         $stmt->store_result();
-        //     }else {
-        //         echo "jd blad";
-        //     }
-        // }
-
-
-        //    $missing = array();
-        //    foreach ($_POST as $key => $value) { if ($value == "") { array_push($missing, $key);}}
-        //    if (count($missing) > 0) {
-        //     echo '<div class="alert alert-danger" role="alert">
-        //                     Proszę wypełnić wszystkie pola
-        //                   </div>';
-        //                   exit;
-        //     } else {
-        //     unset($missing);
-
-        //     $ad_name = $_POST["ad_name"];
-        //     $name =  $_POST["name"];
-        //     $city =  $_POST["city"];
-        //     $address = $_POST["adress"];
-        //     $phone = $_POST["telephone"];
-        //     $email =  $_POST["email"];
-        //     $kitchen =  $_POST["kitchen"];
-        //     $bathroom =  $_POST["bathroom"];
-        //     $number_of_bathrooms =  $_POST["ile-lazienek"];
-        //     $bedroom =  $_POST["bedroom"];
-        //     $number_of_bedrooms = $_POST["ile-sypialni"];
-        //     $garage =  $_POST["garage"];
-        //     $window =  $_POST["window"];
-        //     $washing =  $_POST["pranie-zaslon"];
-        //     $payment =  $_POST["payment-method"];
-        //     $price =  $_POST["cena"];
-
-
-            
-    
-        }
-        ?>
     </div>
         
 
